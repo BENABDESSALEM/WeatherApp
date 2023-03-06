b61a12be866cd8796ee6a02762c51251d2701658
Wajih Benabdessalem <benabdessalemwajih1212@gmail.com>
2023-03-06 14:35:44 +0100

    Add performance to cities list screen

--- WeatherApp/WeatherApp/Screens/CityList/View/CityCell/CityTableViewCell.swift
+++ WeatherApp/WeatherApp/Screens/CityList/View/CityCell/CityTableViewCell.swift
@@ -12,10 +12,10 @@ class CityTableViewCell: UITableViewCell {
     static let cellId = String(describing: CityTableViewCell.self)
     static func nib() -> UINib { return UINib(nibName: cellId, bundle: nil) }
     
-    @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var containerView: UIView!
-    
+    @IBOutlet weak var weatherImageView: UIImageView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
